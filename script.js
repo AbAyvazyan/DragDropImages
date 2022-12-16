3303435a4189ed72e7f955fd4c9783cd1f808d8b
AbAyvazyan333 <108995486+AbAyvazyan333@users.noreply.github.com>
2022-12-16 18:40:49 +0400

    already ready

--- script.js
+++ script.js
@@ -1,91 +1,42 @@
-
-
-function UploadFileHandler({elementID,addID,type=[]}){
+function UploadFileHandler({elementID, addID, type = []}) {
     this.elementID = elementID
     this.addID = addID
-    this.type = type
+    this.type = type;
+    this.files = [];
 
     this.imageArray = []
 
     const takeImageBlock = document.getElementById(elementID)
     const addImages = document.getElementById(addID)
+    const inputedImages = document.getElementById('fileInput')
 
-    function listenerBinder(){
-        takeImageBlock.addEventListener('drop',onDragOut)
-        takeImageBlock.addEventListener('dragover',onDragOver)
-        takeImageBlock.addEventListener('dragleave',onDragLeave)
-        takeImageBlock.addEventListener('dragenter',onDragEnter)
+    function listenerBinder() {
+        takeImageBlock.addEventListener('drop', onDragOut)
+        takeImageBlock.addEventListener('dragover', onDragOver)
+        takeImageBlock.addEventListener('dragleave', onDragLeave)
+        takeImageBlock.addEventListener('dragenter', onDragEnter)
     }
 
+    listenerBinder()
 
-    function onDragEnter(e){
+
+    function onDragEnter(e) {
         e.preventDefault()
     }
 
-    function onDragOver(e){
+    function onDragOver(e) {
         e.preventDefault()
     }
 
-    function onDragLeave(e){
+    function onDragLeave(e) {
         e.preventDefault()
     }
 
-    function onDragOut(e){
-        e.preventDefault()
-
-        // let imageArray = [...e.dataTransfer.files]
-        // const reader = new FileReader()
-        // imageArray.map(image=>{
-
-
-        //     document.onload = function (){
-        //         let cardDiv = document.createElement('div')
-        //         let cardImage = document.createElement('img')
-        //         let cardInfo = document.createElement('div')
-        //         let cardInfoType = document.createElement('span')
-        //         let cardInfoSize = document.createElement('span')
-        //         let cardDelete = document.createElement('span')
-        //
-        //
-        //         cardDiv.className = 'image_card'
-        //
-        //
-        //         cardImage.src = reader.result;
-        //         cardImage.alt = 'Image'
-        //
-        //         cardInfo.className = 'image_info'
-        //
-        //         cardInfoType.innerText = image.type
-        //
-        //         cardInfoSize.innerText = image.size
-        //
-        //         cardDelete.className = 'delete_image_card'
-        //         cardDelete.innerText = '&#10008;'
-        //
-        //
-        //         cardDiv.appendChild(cardImage)
-        //
-        //         cardInfo.append(cardInfoType,cardInfoSize)
-        //
-        //         cardDiv.append(cardInfo,cardDelete)
-        //
-        //
-        //         addImages.appendChild(cardDiv)
-        //
-        //     }
-        //
-        //     reader.readAsDataURL(image)
-        //
-        //
-        // })
-
-        let files = e.dataTransfer.files;
-
-        let file;
-        for (let i=0; i<files.length ; i++){
+    function addImagesHandler(files) {
+        for (let i = 0; i < files.length; i++) {
+            this.files.push(files[i])
             let reader = new FileReader();
-            file = files [i];
-            reader.onload = (file) => {
+            reader.onload = () => {
                 let cardDiv = document.createElement('div')
                 let cardImage = document.createElement('img')
                 let cardInfo = document.createElement('div')
@@ -101,35 +52,53 @@ function UploadFileHandler({elementID,addID,type=[]}){
                 cardImage.alt = 'Image'
 
                 cardInfo.className = 'image_info'
+                console.log(files[i])
+                cardInfoType.innerText = files[i].type.split('/')[1]
 
-                cardInfoType.innerText = file.type
-
-                cardInfoSize.innerText = file.size
+                cardInfoSize.innerText = `(${files[i].size})`
 
                 cardDelete.className = 'delete_image_card'
-                cardDelete.innerText = '&#10008;'
+                cardDelete.innerHTML = '&#10008;'
+
+                cardDelete.addEventListener('click', function (e) {
+                    e.currentTarget.parentNode.remove();
+                })
 
 
                 cardDiv.appendChild(cardImage)
 
-                cardInfo.append(cardInfoType,cardInfoSize)
+                cardInfo.append(cardInfoType, cardInfoSize)
 
-                cardDiv.append(cardInfo,cardDelete)
+                cardDiv.append(cardInfo, cardDelete)
 
 
                 addImages.appendChild(cardDiv)
             }
-            reader.readAsDataURL(file)
+            reader.readAsDataURL(files[i])
         }
-
-
+        console.log(this.files)
     }
 
-    listenerBinder()
+    function onDragOut(e) {
+        e.preventDefault()
 
+
+        let files = e.dataTransfer.files;
+
+        addImagesHandler(files)
+    }
+
+    function fn (e) {
+        let files = e.target.files
+
+        addImagesHandler.bind(this)([...files])
+        e.target.value = ''
+    }
+    inputedImages.addEventListener('change', fn.bind(this))
 }
 
 const uploadFile = new UploadFileHandler({
-    elementID:'take_images',
-    addID:'show_part'
+    elementID: 'take_images',
+    addID: 'show_part',
+    type: ['png']
 })
